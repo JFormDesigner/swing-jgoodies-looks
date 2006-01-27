51fcbf18e38b3662d8cbe80910b65804daa78a0e
golovnin <golovnin@b5711f71-d28f-c463-b624-ad1093da3edd>
2006-01-27 16:03:54 +0000

    Fix for issue #99: Provide AA fallback, in case SwingUtilities2 is absent

--- src/core/com/jgoodies/looks/common/RenderingUtils.java
+++ src/core/com/jgoodies/looks/common/RenderingUtils.java
@@ -31,8 +31,17 @@
 package com.jgoodies.looks.common;
 
 import java.awt.Graphics;
+import java.awt.Graphics2D;
+import java.awt.GraphicsDevice;
+import java.awt.PrintGraphics;
+import java.awt.RenderingHints;
+import java.awt.Toolkit;
+import java.awt.print.PrinterGraphics;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Map;
 
 import javax.swing.JComponent;
 import javax.swing.plaf.basic.BasicGraphicsUtils;
@@ -48,12 +57,15 @@ import com.jgoodies.looks.LookUtils;
  * without further notice.
  *
  * @author  Karsten Lentzsch
- * @version $Revision: 1.4 $
+ * @author  Andrej Golovnin
+ * @version $Revision: 1.5 $
  * 
  * @since 2.0
  */
 public final class RenderingUtils {
-    
+
+    private static final String PROP_DESKTOPHINTS = "awt.font.desktophints";
+
     private static final String SWING_UTILITIES2_NAME =
         LookUtils.IS_JAVA_6_OR_LATER
              ? "sun.swing.SwingUtilities2"
@@ -98,17 +110,27 @@ public final class RenderingUtils {
      */
     public static void drawString(JComponent c, Graphics g, String text, 
             int underlinedChar, int x, int y) {
-        if (LookUtils.IS_JAVA_5_OR_LATER && (drawStringMethod != null)) {
-            try {
-                drawStringMethod.invoke(null, new Object[]{c, g, text, new Integer(x), new Integer(y)});
-                return;
-            } catch (IllegalArgumentException e) {
-                // Use the BasicGraphicsUtils as fallback
-            } catch (IllegalAccessException e) {
-                // Use the BasicGraphicsUtils as fallback
-            } catch (InvocationTargetException e) {
-                // Use the BasicGraphicsUtils as fallback
+        if (LookUtils.IS_JAVA_5_OR_LATER) {
+            if (drawStringMethod != null) {
+                try {
+                    drawStringMethod.invoke(null,
+                            new Object[]{c, g, text, new Integer(x), new Integer(y)});
+                    return;
+                } catch (IllegalArgumentException e) {
+                    // Use the BasicGraphicsUtils as fallback
+                } catch (IllegalAccessException e) {
+                    // Use the BasicGraphicsUtils as fallback
+                } catch (InvocationTargetException e) {
+                    // Use the BasicGraphicsUtils as fallback
+                }
             }
+            Graphics2D g2 = (Graphics2D) g;
+            Map oldRenderingHints = installDesktopHints(g2);
+            BasicGraphicsUtils.drawString(g, text, underlinedChar, x, y);
+            if (oldRenderingHints != null) {
+                g2.addRenderingHints(oldRenderingHints);
+            }
+            return;
         }
         BasicGraphicsUtils.drawString(g, text, underlinedChar, x, y);
     }
@@ -127,17 +149,28 @@ public final class RenderingUtils {
      */
     public static void drawStringUnderlineCharAt(JComponent c,Graphics g,
                            String text, int underlinedIndex, int x,int y) {
-        if (LookUtils.IS_JAVA_5_OR_LATER && (drawStringUnderlineCharAtMethod != null)) {
-            try {
-                drawStringUnderlineCharAtMethod.invoke(null, new Object[]{c, g, text, new Integer(underlinedIndex), new Integer(x), new Integer(y)});
-                return;
-            } catch (IllegalArgumentException e) {
-                // Use the BasicGraphicsUtils as fallback
-            } catch (IllegalAccessException e) {
-                // Use the BasicGraphicsUtils as fallback
-            } catch (InvocationTargetException e) {
-                // Use the BasicGraphicsUtils as fallback
+        if (LookUtils.IS_JAVA_5_OR_LATER) {
+            if (drawStringUnderlineCharAtMethod != null) {
+                try {
+                    drawStringUnderlineCharAtMethod.invoke(null,
+                            new Object[] {c, g, text, new Integer(underlinedIndex),
+                                          new Integer(x), new Integer(y)});
+                    return;
+                } catch (IllegalArgumentException e) {
+                    // Use the BasicGraphicsUtils as fallback
+                } catch (IllegalAccessException e) {
+                    // Use the BasicGraphicsUtils as fallback
+                } catch (InvocationTargetException e) {
+                    // Use the BasicGraphicsUtils as fallback
+                }
             }
+            Graphics2D g2 = (Graphics2D) g;
+            Map oldRenderingHints = installDesktopHints(g2);
+            BasicGraphicsUtils.drawStringUnderlineCharAt(g, text, underlinedIndex, x, y);
+            if (oldRenderingHints != null) {
+                g2.addRenderingHints(oldRenderingHints);
+            }
+            return;
         }
         BasicGraphicsUtils.drawStringUnderlineCharAt(g, text, underlinedIndex, x, y);
     }
@@ -180,6 +213,52 @@ public final class RenderingUtils {
         }
         return null;
     }
-    
-    
+
+
+    private static Map installDesktopHints(Graphics2D g2) {
+        Map oldRenderingHints = null;
+        if (LookUtils.IS_JAVA_6_OR_LATER) {
+            Map desktopHints = desktopHints(g2);
+            if (desktopHints != null && !desktopHints.isEmpty()) {
+                oldRenderingHints = new HashMap(desktopHints.size());
+                RenderingHints.Key key;
+                for (Iterator i = desktopHints.keySet().iterator(); i.hasNext();) {
+                    key = (RenderingHints.Key) i.next();
+                    oldRenderingHints.put(key, g2.getRenderingHint(key));
+                }
+                g2.addRenderingHints(desktopHints);
+            }
+        }
+        return oldRenderingHints;
+    }
+
+
+    private static Map desktopHints(Graphics2D g2) {
+        if (isPrinting(g2)) {
+            return null;
+        }
+        Toolkit toolkit = Toolkit.getDefaultToolkit();
+        GraphicsDevice device = g2.getDeviceConfiguration().getDevice();
+        Map desktopHints = (Map) toolkit.getDesktopProperty(
+                PROP_DESKTOPHINTS + '.' + device.getIDstring());
+        if (desktopHints == null) {
+            desktopHints = (Map) toolkit.getDesktopProperty(PROP_DESKTOPHINTS);
+        }
+        // It is possible to get a non-empty map but with disabled AA.
+        if (desktopHints != null) {
+            Object aaHint = desktopHints.get(RenderingHints.KEY_TEXT_ANTIALIASING);
+            if ((aaHint == RenderingHints.VALUE_TEXT_ANTIALIAS_OFF) ||
+                (aaHint == RenderingHints.VALUE_TEXT_ANTIALIAS_DEFAULT)) {
+                desktopHints = null;
+            }
+        }
+        return desktopHints;
+    }
+
+
+    private static boolean isPrinting(Graphics g) {
+        return g instanceof PrintGraphics || g instanceof PrinterGraphics;
+    }
+
+
 }
