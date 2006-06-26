3a028905667b74bf536d59cfcf500132707d1304
karsten <karsten@b5711f71-d28f-c463-b624-ad1093da3edd>
2006-06-26 18:36:17 +0000

    Fixed a JavaDoc tag; improved docs

--- src/core/com/jgoodies/looks/common/RGBGrayFilter.java
+++ src/core/com/jgoodies/looks/common/RGBGrayFilter.java
@@ -1,6 +1,6 @@
 /*
  * Copyright (c) 2001-2006 JGoodies Karsten Lentzsch. All Rights Reserved.
- *
+ * 
  * Redistribution and use in source and binary forms, with or without 
  * modification, are permitted provided that the following conditions are met:
  * 
@@ -48,7 +48,7 @@ import javax.swing.ImageIcon;
  *
  * @author Sun
  * @author Andrej Golovnin
- * @version $Revision: 1.1 $
+ * @version $Revision: 1.2 $
  */
 public final class RGBGrayFilter extends RGBImageFilter {
 
@@ -76,7 +76,7 @@ public final class RGBGrayFilter extends RGBImageFilter {
         } else {
             img = new BufferedImage(icon.getIconWidth(), icon.getIconHeight(),
                     BufferedImage.TYPE_INT_ARGB);
-            // TODO: can we call this method when component is null???
+            // TODO: Clarify if we call #paintIcon with a null component.
             icon.paintIcon(component, img.getGraphics(), 0, 0);
         }
 
@@ -90,27 +90,28 @@ public final class RGBGrayFilter extends RGBImageFilter {
     /**
      * Converts a single input pixel in the default RGB ColorModel to a single
      * gray pixel.
-     * @param x,&nbsp;y the coordinates of the pixel
-     * @param rgb the integer pixel representation in the default RGB
-     *            color model
+     * 
+     * @param x    the horizontal pixel coordinate
+     * @param y    the vertical pixel coordinate
+     * @param rgb  the integer pixel representation in the default RGB color model
      * @return a gray pixel in the default RGB color model.
+     * 
      * @see ColorModel#getRGBdefault
      * @see #filterRGBPixels
      */
     public int filterRGB(int x, int y, int rgb) {
-        // find the average of red, green, and blue
+        // Find the average of red, green, and blue.
         float avg = (((rgb >> 16) & 0xff) / 255f +
                      ((rgb >>  8) & 0xff) / 255f +
                       (rgb        & 0xff) / 255f) / 3;
-        // pull out the alpha channel
+        // Pull out the alpha channel.
         float alpha = (((rgb >> 24) & 0xff) / 255f);
-        // calc the average
-        // Sun's original formula:
-        // avg = Math.min(1.0f, (1f - avg) / (100.0f / 35.0f) + avg);
-        // Andrej Golovnin: JGoodies formula does the same as above but uses
-        // less operations and hence is faster.
+        
+        // Calculate the average.
+        // Sun's formula: Math.min(1.0f, (1f - avg) / (100.0f / 35.0f) + avg);
+        // Andrej: the following formula uses less operations and hence is faster.
         avg = Math.min(1.0f, 0.35f + 0.65f * avg);
-        // turn back into rgb
+        // Turn back into RGB.
         int rgbval = (int) (alpha * 255f) << 24 |
                      (int) (avg   * 255f) << 16 |
                      (int) (avg   * 255f) << 8  |
