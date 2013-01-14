8cc1c99187f0aaeb58a7a354ac1f801be4d9e735
karsten <karsten@b5711f71-d28f-c463-b624-ad1093da3edd>
2013-01-14 18:53:44 +0000

    Updated copyright date

--- src/main/java/com/jgoodies/looks/common/ShadowPopup.java
+++ src/main/java/com/jgoodies/looks/common/ShadowPopup.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2007-2012 JGoodies Software GmbH. All Rights Reserved.
+ * Copyright (c) 2007-2013 JGoodies Software GmbH. All Rights Reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
